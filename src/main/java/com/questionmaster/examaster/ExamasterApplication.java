03c84f1649246c449523de81ef9374607cfa2898
yumaad <yudorzhieva@gmail.com>
2025-04-27 23:45:15 +0300

    Creating application

---
+++ src/main/java/com/questionmaster/examaster/ExamasterApplication.java
@@ -0,0 +1,13 @@
+package com.questionmaster.examaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/questionmaster/examaster/ExamasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questionmaster.examaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
